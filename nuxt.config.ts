7dfee60a6edb90bc400a936ba82184fce56147dd
den <khausden@gmail.com>
2023-08-14 19:49:02 +1000

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    prerender: {
+      ignore: ["/otp"],
+    },
+  },
   app: {
     baseURL: "/auth/",
   },
